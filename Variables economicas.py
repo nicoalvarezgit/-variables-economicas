f7118f27df6b2a4215269e14f42fef711faa4713
Nicolas Alvarez <niconte3500@hotmail.com>
2024-10-13 17:01:26 -0300

    Add files via upload

---
+++ Variables economicas.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  4 16:30:24 2024
+
+@author: L12504
+"""
+
+import requests
+import pandas as pd
+
+
+url = "https://api.bcra.gob.ar/estadisticas/v2.0/principalesvariables"
+
+response = requests.get(url)
+data = response.json()
+
+results = data['results']
+
+pv = pd.DataFrame(results)
+
+print(pv.head())
---
+++ readme.md
@@ -0,0 +1,4 @@
+Este es el readme del trabajo de variables económicas.
+
+El objetivo es armar un set de datos con variables ecónomicas para interpretar la realidad nacional.
+
