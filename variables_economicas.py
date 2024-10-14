399318e49dde843522cbd7f43abf6312d321acea
nico <niconte3500@hotmail.com>
2024-10-14 14:41:20 -0300

    Se aregan las claves al script

--- variables_economicas.py
+++ variables_economicas.py
@@ -4,10 +4,20 @@ Created on Wed Sep  4 16:30:24 2024
 
 @author: L12504
 """
-
+import os
+from dotenv import load_dotenv
 import requests
 import pandas as pd
 
+# Se cargan las variables del archivo .env
+load_dotenv()
+
+# Obtener las credenciales desde las variables de entorno
+user = os.getenv('REDSHIFT_USER')
+password = os.getenv('REDSHIFT_PASSWORD')
+host = os.getenv('REDSHIFT_HOST')
+port = os.getenv('REDSHIFT_PORT')
+database = os.getenv('REDSHIFT_DB')
 
 url = "https://api.bcra.gob.ar/estadisticas/v2.0/principalesvariables"
 
