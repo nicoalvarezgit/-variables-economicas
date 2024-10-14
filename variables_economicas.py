cbb9e8b5081b6a7c0ac09e72bb02c160e55078b9
nico <niconte3500@hotmail.com>
2024-10-14 02:47:42 -0300

    cambio de nombre archivo principal

---
+++ variables_economicas.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  4 16:30:24 2024
+
+@author: L12504
+"""
+
+import requests
+import pandas as pd
+
+
+url = "https://api.bcra.gob.ar/estadisticas/v2.0/principalesvariables"
+
+response = requests.get(url, verify=False)
+data = response.json()
+
+results = data['results']
+
+listado_variables = pd.DataFrame(results)
+
+print(listado_variables.head())
